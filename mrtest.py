6eb812216869edfad306c84cce27560d28a1414d
Julien-ser <73262183+Julien-ser@users.noreply.github.com>
2022-07-05 11:04:38 -0400

    Update mrtest.py

--- mrtest.py
+++ mrtest.py
@@ -3,6 +3,7 @@ import streamlit as st
 import mediapipe as mp
 import Hand_track as h
 from PIL import Image
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
 detector = h.handDetector(maxHands = 1, detectionCon = 0.75)
 
@@ -13,63 +14,67 @@ cam = cv2.VideoCapture(0)
 
 placeholder = st.empty()
 
-while run:
-    ret, img = cam.read()
-    img = detector.findHands(img, True)
-    img = cv2.flip(img, 1)
+class VideoTransformer(VideoTransformerBase):
+    
+    global placeholder
+    
+    def __init__(self):
+        self.i = 0
 
-    #print(img.shape)
-    lmLis = detector.findPos(img)
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        img = detector.findHands(img, True)
+        img = cv2.flip(img, 1)
 
-    #img[0:200, 0:200] = overlay[0]
+        #print(img.shape)
+        lmLis = detector.findPos(img)
 
-    x = []
-    y = []
+        #img[0:200, 0:200] = overlay[0]
 
-    if len(lmLis) != 0:
-            
-        
-        for id, xa, ya in lmLis:
-            x.append(xa)
-            y.append(ya)
+        x = []
+        y = []
 
-        min_x = min(x)
-        min_y = min(y)
-        max_x = max(x)
-        max_y = max(y)
+        if len(lmLis) != 0:
 
-        #cv2.rectangle(img, (min_x, min_y), (max_x, max_y), (0, 255, 255), 2)
 
-        try:
-            if lmLis[8][2] > lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
-                #cv2.putText(img, 'Fuck you', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
-                placeholder.empty()
-                placeholder.text('Middle finger 🖕')
+            for id, xa, ya in lmLis:
+                x.append(xa)
+                y.append(ya)
 
-            if lmLis[8][2] < lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
-                #cv2.putText(img, 'Peace', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
-                placeholder.empty()
-                placeholder.text('Peace ✌')
+            min_x = min(x)
+            min_y = min(y)
+            max_x = max(x)
+            max_y = max(y)
 
-            if lmLis[8][2] > lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] < lmLis[12][2]:
-                if lmLis[4][2] < lmLis[6][2]:
-                    #cv2.putText(img, 'Thumbs up', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+            #cv2.rectangle(img, (min_x, min_y), (max_x, max_y), (0, 255, 255), 2)
+
+            try:
+                if lmLis[8][2] > lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
+                    #cv2.putText(img, 'Fuck you', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
                     placeholder.empty()
-                    placeholder.text('Thumbs Up 👍')
-                else:
-                    #cv2.putText(img, 'Fist', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+                    placeholder.text('Middle finger 🖕')
+
+                if lmLis[8][2] < lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
+                    #cv2.putText(img, 'Peace', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
                     placeholder.empty()
-                    placeholder.text('Fist 👊')
+                    placeholder.text('Peace ✌')
 
-            if lmLis[8][2] < lmLis[6][2] and lmLis[16][2] < lmLis[14][2] and lmLis[20][2] < lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
-                if abs(abs(lmLis[12][1] - lmLis[16][1])/(abs(lmLis[12][1] - lmLis[8][1])) + 1) >= 3 and abs(abs(lmLis[12][1] - lmLis[16][1])/(abs(lmLis[20][1] - lmLis[16][1]) + 1)) >= 3:
-                    #cv2.putText(img, 'Vulcans', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
-                    placeholder.text('Vulcan salute 🖖')
-        except:
-            pass
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(img)
+                if lmLis[8][2] > lmLis[6][2] and lmLis[16][2] > lmLis[14][2] and lmLis[20][2] > lmLis[18][2] and lmLis[10][2] < lmLis[12][2]:
+                    if lmLis[4][2] < lmLis[6][2]:
+                        #cv2.putText(img, 'Thumbs up', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+                        placeholder.empty()
+                        placeholder.text('Thumbs Up 👍')
+                    else:
+                        #cv2.putText(img, 'Fist', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+                        placeholder.empty()
+                        placeholder.text('Fist 👊')
 
-else:
-    placeholder.empty()
-    placeholder.text('Stopped')
+                if lmLis[8][2] < lmLis[6][2] and lmLis[16][2] < lmLis[14][2] and lmLis[20][2] < lmLis[18][2] and lmLis[10][2] > lmLis[12][2]:
+                    if abs(abs(lmLis[12][1] - lmLis[16][1])/(abs(lmLis[12][1] - lmLis[8][1])) + 1) >= 3 and abs(abs(lmLis[12][1] - lmLis[16][1])/(abs(lmLis[20][1] - lmLis[16][1]) + 1)) >= 3:
+                        #cv2.putText(img, 'Vulcans', (0, 0), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255), 3)
+                        placeholder.text('Vulcan salute 🖖')
+            except:
+                pass
+        return img
+    
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
